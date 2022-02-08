f7e59b6a4f2a78f64b85a327602da38935a9269a
markus <marklogist@yandex.ru>
2022-02-08 21:55:56 +1000

    'ifelse'

--- js/script.js
+++ js/script.js
@@ -9,10 +9,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов? ', ''),
-      b = prompt('На сколько вы его оцените ?', ''),
-      c = prompt('Один из последних просмотренных фильмов? ', ''),
-      d = prompt('На сколько вы его оцените ?', '');
 
-      personalMovieDB.movies[a] = [b];
-      personalMovieDB.movies[c] = [d];
+      for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов? ', ''),
+              b = prompt('На сколько вы его оцените ?', '');
+              personalMovieDB.movies[a] = [b];
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = [b];
+      } else {
+          console.log('error');
+          i--;
+      }
+    }
+
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотренно довольно таки мало фильмов!');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('вы классный зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('вы просто киноман');
+    } else {
+        console.log('Произошла ошибка!');
+    }
+
+console.log(personalMovieDB);
